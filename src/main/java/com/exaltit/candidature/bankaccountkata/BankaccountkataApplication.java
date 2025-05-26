a863056555750ad4d3b3dc7d1feb4498fb689e34
Ahmed D. MK <ahmedmkhinini@gmail.com>
2025-05-26 14:21:04 +0200

    first commit

---
+++ src/main/java/com/exaltit/candidature/bankaccountkata/BankaccountkataApplication.java
@@ -0,0 +1,13 @@
+package com.exaltit.candidature.bankaccountkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaccountkataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaccountkataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exaltit/candidature/bankaccountkata/BankaccountkataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exaltit.candidature.bankaccountkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaccountkataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
